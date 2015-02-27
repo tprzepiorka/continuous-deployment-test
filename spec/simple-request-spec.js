59bfe64b0f73ee86c304d9bd1df6c123a3cb1710
Thomas Przepiorka <thomas.przepiorka11@imperial.ac.uk>
2015-02-27 11:44:30 +0000

    tolocalhost

--- spec/simple-request-spec.js
+++ spec/simple-request-spec.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 describe("a simple test suite", function() {
 	it("should repond with 'Hello Niket!'", function(done) {
-		request("http://dev.circleci.com:3000/name/Niket", function(error, response, body) {
+		request("http://localhost:3000/name/Niket", function(error, response, body) {
 			console.log(error);
 			console.log("request: " + JSON.stringify(response));
 			expect(body).toEqual("Hello Niket!\n\n");
